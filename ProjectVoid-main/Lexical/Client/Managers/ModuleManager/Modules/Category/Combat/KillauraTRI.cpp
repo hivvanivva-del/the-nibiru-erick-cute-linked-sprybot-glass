3d1e5599fb8768c762c2d422fb391476b9b6f903
erickkxd01-lab <erickkxd69@gmail.com>
2026-02-09 16:22:02 -0600

    1st commit

--- ProjectVoid-main/Lexical/Client/Managers/ModuleManager/Modules/Category/Combat/KillauraTRI.cpp
+++ ProjectVoid-main/Lexical/Client/Managers/ModuleManager/Modules/Category/Combat/KillauraTRI.cpp
@@ -1,2 +1,13 @@
 #include "KillauraTRI.h"
-#include "../../../ModuleManager.h"
+
+KillauraTRI::KillauraTRI() : Module("KillauraTRI", "Advanced TRI-based KillAura", Category::COMBAT) {
+    // Initial implementation
+}
+
+void KillauraTRI::onNormalTick(LocalPlayer* localPlayer) {
+    // Logic goes here
+}
+
+void KillauraTRI::onUpdateRotation(LocalPlayer* localPlayer) {
+    // Rotation logic goes here
+}
--- ProjectVoid-main/Lexical/Client/Managers/ModuleManager/Modules/Category/Combat/KillauraTRI.h
+++ ProjectVoid-main/Lexical/Client/Managers/ModuleManager/Modules/Category/Combat/KillauraTRI.h
@@ -1,1 +1,10 @@
-#include "../../../ModuleManager.h"
+#pragma once
+#include "../../ModuleBase/Module.h"
+
+class KillauraTRI : public Module {
+public:
+    KillauraTRI();
+
+    void onNormalTick(LocalPlayer* localPlayer) override;
+    void onUpdateRotation(LocalPlayer* localPlayer) override;
+};
